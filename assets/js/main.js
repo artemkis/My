97222ed393e881aec30ca76c866501770d9d654a
Artem <artemkis2015@gmail.com>
2026-03-04 18:27:38 +0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('a[href="go"]').forEach((elem) => {
+  elem.onclick = function (e) {
+    e.preventDefault();
+    window.location.href = "https://redirecttraff.site/F8MXzKYT";
+    return false;
+  };
+});
